455b9793ac9a1414a1f9fe7498eec60bea2a6806
Charlie <charliej9423@gmail.com>
2024-11-24 23:49:35 +1100

    Final iteration of servo test code

--- servo_test/servo_test.ino
+++ servo_test/servo_test.ino
@@ -9,19 +9,18 @@
 
 #include <Servo.h>
 
-Servo yaw;  // create servo object to control a servo
+Servo yaw;  // initialise server objects for both dimensions
 Servo pitch;
-// twelve servo objects can be created on most boards
 
-int pos = 0;    // variable to store the servo position
-int yawValue;
+
+int yawValue; // initialise variables to store the coordinates
 int pitchValue;
-int yawPotLeft = A0;
+int yawPotLeft = A0; //initialise input pins
 int pitchPotRight = A1;
 
 
 void setup() {
-  yaw.attach(9);  // attaches the servo on pin 9 to the servo object
+    yaw.attach(9);  // attaches servo objects to pins
   pitch.attach(10);
 }
 
@@ -31,6 +30,6 @@ void loop() {
   yawValue = map(yawValue, 0, 1023, 0, 180);     // scale it for use with the servo (value between 0 and 180)
   pitchValue = map(pitchValue, 0, 1023, 0, 180);     // scale it for use with the servo (value between 0 and 180)
   delay(15);                           // waits for the servo to get there
-  yaw.write(yawValue);              // tell servo to go to position in variable 'pos'
+  yaw.write(yawValue);              // tell servo to go to position
   pitch.write(pitchValue);
 }
