72a94017dfd703ad430122713bae396b2deabdfd
Charlie <charliej9423@gmail.com>
2024-11-26 09:14:44 +1100

    Added new test for serial communication

---
+++ servo_test/serial_argument_passing/serial_argument_passing.ino
@@ -0,0 +1,83 @@
+// Example 5 - Receive with start- and end-markers combined with parsing
+
+const byte numChars = 32;
+char receivedChars[numChars];
+char tempChars[numChars];        // temporary array for use when parsing
+
+      // variables to hold the parsed data
+char messageFromPC[numChars] = {0};
+int integerFromPC = 0;
+float floatFromPC = 0.0;
+
+boolean newData = false;
+
+//============
+
+void setup() {
+    Serial.begin(9600);
+    Serial.println("This demo expects 2 ints <int1,int2>");
+
+}
+
+//============
+
+void loop() {
+    recvWithStartEndMarkers();
+    if (newData == true) {
+        strcpy(tempChars, receivedChars);
+            // this temporary copy is necessary to protect the original data
+            //   because strtok() used in parseData() replaces the commas with \0
+        parseData();
+        newData = false;
+    }
+}
+
+//============
+
+void recvWithStartEndMarkers() {
+    static boolean recvInProgress = false;
+    static byte ndx = 0;
+    char startMarker = '<';
+    char endMarker = '>';
+    char rc;
+
+    while (Serial.available() > 0 && newData == false) {
+        rc = Serial.read();
+
+        if (recvInProgress == true) {
+            if (rc != endMarker) {
+                receivedChars[ndx] = rc;
+                ndx++;
+                if (ndx >= numChars) {
+                    ndx = numChars - 1;
+                }
+            }
+            else {
+                receivedChars[ndx] = '\0'; // terminate the string
+                recvInProgress = false;
+                ndx = 0;
+                newData = true;
+            }
+        }
+
+        else if (rc == startMarker) {
+            recvInProgress = true;
+        }
+    }
+}
+
+//============
+
+void parseData() {      // split the data into its parts,
+// turns data in form x,y into integers with for yaw and pitch
+
+    char * currentSlice; // this is used by strtok() as an index
+
+    currentSlice = strtok(tempChars, ","); // extract int 1
+    yaw = atoi(currentSlice);     // convert 
+
+    currentSlice = strtok(NULL, ","); // extract int 2
+    pitch = atoi(currentSlice);     // convert
+
+}
+
